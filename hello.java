53eb7f8e4b4bc2f1c5b438756d8f83bbd5c0c7df
ifntsh <99662127+ifntsh@users.noreply.github.com>
2024-07-19 15:42:41 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     //자바 프로그램의 진입점(entry point)인 main 메서드를 정의합니다. 이 메서드는 프로그램이 실행될 때 자동으로 호출됩니다. 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //
     }
 }
